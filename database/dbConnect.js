61665789ae82257ba0407baa7d1cfde7d1e4f36a
Shivam Panseja <shivam.panseja@thewitslab.com>
2025-12-10 17:30:23 +0530

    feat/db issue

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose')
  const  dbConnect = async () => {
     try {
        await mongoose.connect(process.env.MONGO_URL)
-       console.log('Databse connected')
+       console.log('Databse connected', process.env.MONGO_URL)
     }catch (error) {
         console.log('Database error')
+        console.log('Database error', process.env.MONGO_URL)
     }
 }
 module.exports = dbConnect
